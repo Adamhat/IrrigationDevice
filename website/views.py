d2280e7b043e04468a95ef242885a86d606ac9b8
Adam <adamfarhat129@gmail.com>
2023-01-04 11:27:59 -0800

    Setting up general login page and front end.
    Signed-off-by: Adam <adamfarhat129@gmail.com>

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # This file is where we store the information that users can access
 views = Blueprint('views', __name__)
 
 @views.route('Home') 
 def home(): # Whenever we access the main page of our website, whatever is in home will run
-    return "<h1>Test</h1>"
+    return render_template("home.html")
