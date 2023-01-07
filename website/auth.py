0ab74c2ab3e67f7d7afa6117d5a1342776a359ff
Adam <adamfarhat129@gmail.com>
2023-01-06 17:55:57 -0800

    Added error detecting
    Signed-off-by: Adam <adamfarhat129@gmail.com>

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 # This file is where we store the login information if need be
 auth = Blueprint('auth', __name__)
@@ -13,4 +13,26 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 3:
+            flash('Email must be greater then 3 characters.', category='error')
+            pass
+        elif len(firstName) < 1:
+            flash('First name must be greater then 1 character.', category='error')
+            pass
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+            pass
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+            pass
+        else:
+            flash('Account created!.', category='success')
+            pass
+
     return render_template("sign_up.html")
